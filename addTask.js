e58cb360df39d0262cacf7b86ca008ac775f62b6
warren-b-chen <49181236+warren-b-chen@users.noreply.github.com>
2023-02-15 16:07:49 -0700

    use anon func to call func for event listerner. Idk if this even adds a branch

--- addTask.js
+++ addTask.js
@@ -5,73 +5,54 @@ let taskRepeat = false;
 //add task name into vals
 add_task.addEventListener('click', () => {
   const task_name = document.querySelector('#task').value;
-  vals.append(task_name);
+  vals.push(task_name);
 });
 
 //make time options visible and select timeframe
 const timeChosen = document.querySelector('#time-select');
 timeChosen.addEventListener('change', (event)=>{
-  console.log(event.target.value);
-  if (event.target.value === 'daily') {
-    document.getElementById('year-container').hidden = true;
-    document.getElementById('month-container').hidden = true;
-    document.getElementById('dailies-container').hidden = false;
-  }
-  else if(event.target.value === ""){
-    document.getElementById('year-container').hidden = true;
-    document.getElementById('month-container').hidden = true;
-    document.getElementById('dailies-container').hidden = true;
-  }
-  else if(event.target.value==="monthly"){
-    document.getElementById('year-container').hidden = true;
-    document.getElementById('month-container').hidden = false;
-    document.getElementById('dailies-container').hidden = true;
-  }
-  else if(event.target.value==="yearly"){
-    document.getElementById('year-container').hidden = false;
-    document.getElementById('month-container').hidden = true;
-    document.getElementById('dailies-container').hidden = true;
-  }
-  //need to get time values into val
-  vals.append(event.target.value);
+  displayCalendar(event)
+  getRepeat();
 });
-  //need to get repeat, but also make sure no other checkbox is ticked
   //need to set the repeat (another func?)
 
+//need to get repeat, but also make sure no other checkbox is ticked
+function getRepeat(){
+  console.log("hi")
+}
 function addTask(task){
   localStorage.setItem(task, JSON.stringify(vals));
 }
 
 function getTask(task){
-let curr_task = localStorage.getItem(task)
+  let curr_task = localStorage.getItem(task)
 }
 
 
 
-function displayCalendar(event){
-  console.log(event.target.value);
-  if (event.target.value === 'daily') {
+function displayCalendar(thing){
+  if (thing.target.value === 'daily') {
     document.getElementById('year-container').hidden = true;
     document.getElementById('month-container').hidden = true;
     document.getElementById('dailies-container').hidden = false;
   }
-  else if(event.target.value === ""){
+  else if(thing.target.value === ""){
     document.getElementById('year-container').hidden = true;
     document.getElementById('month-container').hidden = true;
     document.getElementById('dailies-container').hidden = true;
   }
-  else if(event.target.value==="monthly"){
+  else if(thing.target.value==="monthly"){
     document.getElementById('year-container').hidden = true;
     document.getElementById('month-container').hidden = false;
     document.getElementById('dailies-container').hidden = true;
   }
-  else if(event.target.value==="yearly"){
+  else if(thing.target.value==="yearly"){
     document.getElementById('year-container').hidden = false;
     document.getElementById('month-container').hidden = true;
     document.getElementById('dailies-container').hidden = true;
   }
   //need to get time values into val
-  vals.append(event.target.value);
+  vals.push(thing.target.value);
 };
 
  // Problem: tried to call via: displayCalendar(event), which the callback uses the return function
