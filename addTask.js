468547080e0038eb53afd8c3fa752fbe2071b0b4
warren-b-chen <49181236+warren-b-chen@users.noreply.github.com>
2023-02-15 15:35:29 -0700

    added functionality of containers hiding/appearing upon click, cleaned up classes/id conflicts and names, changed to having vals as array to be passed into localStorage

--- addTask.js
+++ addTask.js
@@ -1,33 +1,72 @@
 const add_task = document.querySelector('#enter');
+let vals = []
+let taskRepeat = false;
+
+//add task name into vals
 add_task.addEventListener('click', () => {
-const task_name = document.querySelector('#task').value;
-  addTask(task_name);
-  getTask(task_name);
+  const task_name = document.querySelector('#task').value;
+  vals.append(task_name);
 });
 
+//make time options visible and select timeframe
+const timeChosen = document.querySelector('#time-select');
+timeChosen.addEventListener('change', (event)=>{
+  console.log(event.target.value);
+  if (event.target.value === 'daily') {
+    document.getElementById('year-container').hidden = true;
+    document.getElementById('month-container').hidden = true;
+    document.getElementById('dailies-container').hidden = false;
+  }
+  else if(event.target.value === ""){
+    document.getElementById('year-container').hidden = true;
+    document.getElementById('month-container').hidden = true;
+    document.getElementById('dailies-container').hidden = true;
+  }
+  else if(event.target.value==="monthly"){
+    document.getElementById('year-container').hidden = true;
+    document.getElementById('month-container').hidden = false;
+    document.getElementById('dailies-container').hidden = true;
+  }
+  else if(event.target.value==="yearly"){
+    document.getElementById('year-container').hidden = false;
+    document.getElementById('month-container').hidden = true;
+    document.getElementById('dailies-container').hidden = true;
+  }
+  //need to get time values into val
+  vals.append(event.target.value);
+});
+  //need to get repeat, but also make sure no other checkbox is ticked
+  //need to set the repeat (another func?)
+
 function addTask(task){
-    console.log(task);
-    localStorage.setItem(task, task);
+  localStorage.setItem(task, JSON.stringify(vals));
 }
 
 function getTask(task){
-  let curr_task = localStorage.getItem(task)
+let curr_task = localStorage.getItem(task)
 }
 
-const timeChosen = document.querySelector('#time-select');
-timeChosen.addEventListener('change', displayCalendar());
 
-function displayCalendar(){
-  if (timeChosen.target.value === 'daily') {
-    document.getElementById('month-container').hidden = true;
-    document.getElementById('day-container').hidden = false;
+
+/*function displayCalendar(event){
+  const timeMenu = document.getElementById("time-select");
+  console.log(event.target.value);
+  if (event.target.value === 'daily') {
+    document.getElementById('month-year-container').hidden = true;
+    document.getElementById('dailies-container').hidden = false;
   }
-  else {
-    document.getElementById('day-container').hidden = true;
-    document.getElementById('month-container').hidden = false;
+  else if(event.target.value === ""){
+    document.getElementById('month-year-container').hidden = true;
+    document.getElementById('dailies-container').hidden = true;
   }
-  console.log(timeChosen.value);
-}
+  else{
+    document.getElementById('dailies-container').hidden = true;
+    document.getElementById('month-year-container').hidden = false;
+  }
+
+  Problem: tried to call via: displayCalendar(event), which the callback uses the return function
+  Solution: call w/: displayCalendar, this actually calls function
+}*/
 
 
 function clearStorage(){
